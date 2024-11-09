b0df231a6cdcf8673f01365a6af4b2e7652a2fb0
joaovitorgraf <grafjoaovitor@gmail.com>
2024-11-08 21:53:52 -0300

    index

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 
 
-<div class="container">
-    <div class="row" id="retangulo">
-        <div class="imagem">
-        <img src="img/teste.png"></img>
+<div class="container position-absolute top-50 start-50 translate-middle">
+    <div class="row">
+        <div class="col-6 " style="background-color: blue; align-itens: center;">
+            <div class="border border-black rounded-5" style="background-color: yellow; width: 400px; height: 550px;">
+                <p>Nome Fantasia</p>
+            </div>
+        </div>
+        <div class="col-6 px-5 py-5" style="background-color: red">
+            <div class="row mb-5">
+                <div style="background-color: yellow;">
+                    <p>Nome Fantasia</p>
+                </div>
+            </div>
+            <div class="row">
+                <div style="background-color: yellow;">
+                    <p>Nome Fantasia</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
