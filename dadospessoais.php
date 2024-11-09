988143983af55fe3ecc61fbf2f3d64b5407a0ddd
iamthais <stanchackthais@gmail.com>
2024-11-08 21:19:42 -0300

    commit

---
+++ dadospessoais.php
@@ -0,0 +1,52 @@
+<?php include "header.php" ?>
+
+<div class="container">
+  <div class="row mt-5 mb-5 justify-content-center flex-nowrap">
+    <div class="col-auto">
+      <button type="button" class="btn btn-primary rounded-0">Dados Pessoais</button>
+    </div>
+    <div class="col-auto">
+      <button type="button" class="btn btn-primary rounded-0 disabled">Sintomas</button>
+    </div>
+    <div class="col-auto">
+      <button type="button" class="btn btn-primary rounded-0 disabled">Questionário</button>
+    </div>
+  </div>
+
+
+
+
+
+
+  <div class="text-center" style="margin-top: 10vh;">
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <input class="form-control mb-4" type="text" placeholder="Nome" aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="Data de Nascimento"
+          aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="Sexo" aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="CPF" aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="Cartão SUS" aria-label="default input example">
+
+      </div>
+      <div class="col-12 col-md-6">
+        <input class="form-control mb-4" type="text" placeholder="CEP" aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="Rua" aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="Número" aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="Bairro" aria-label="default input example">
+        <input class="form-control mb-4" type="text" placeholder="Cidade" aria-label="default input example">
+      </div>
+
+      <div class="col-12 col-md-12">
+        <div class="col-auto text-end">
+          <form action="sintomas.php">
+            <button type="submit" class="btn btn-primary">Confirm identity</button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+<?php include "footer.php"; ?>
--- sintomas.php
+++ sintomas.php
@@ -1,142 +1,154 @@
 <?php include "header.php" ?>
 
-<div class="row mt-5 mb-5 " style="justify-content: center">
-    <div class="btn-group" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-primary disabled">Dados Pessoais</button>
-        <button type="button" class="btn btn-primary ">Sintomas</button>
-        <button type="button" class="btn btn-primary disabled">Questionário</button>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-12 col-md-4 p-3">
-        <div class="form-check fs-5 border rounded-1 p-0">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-                Default checkbox
-            </label>
+<div class="container">
+    <div class="row mt-5 mb-5 justify-content-center flex-nowrap">
+        <div class="col-auto">
+            <button type="button" class="btn btn-primary rounded-0 disabled">Dados Pessoais</button>
         </div>
-    </div>
-    <div class="col-12 col-md-4 p-3">
-        <div class="form-check fs-5 border rounded-1 p-0">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-                Default checkbox
-            </label>
+        <div class="col-auto">
+            <button type="button" class="btn btn-primary rounded-0">Sintomas</button>
         </div>
-    </div>
-    <div class="col-12 col-md-4 p-3">
-        <div class="form-check fs-5 border rounded-1 p-0">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-                Default checkbox
-            </label>
-        </div>
-    </div>
-    <div class="col-12 col-md-4 p-3">
-        <div class="form-check fs-5 border rounded-1 p-0">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-                Default checkbox
-            </label>
-        </div>
-    </div>
-    <div class="col-12 col-md-4 p-3">
-        <div class="form-check fs-5 border rounded-1 p-0">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-                Default checkbox
-            </label>
+        <div class="col-auto">
+            <button type="button" class="btn btn-primary rounded-0 disabled">Questionário</button>
         </div>
     </div>
 
 
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
+    <div class="row">
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
 
 
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-<div class="col-12 col-md-4 p-3">
-    <div class="form-check fs-5 border rounded-1 p-0">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-            Default checkbox
-        </label>
-    </div>
-</div>
-</div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
 
 
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+            <div class="form-check fs-5 border rounded-1 p-0">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+            </div>
+        </div>
+    </div>
 
+    <div class="col-12 col-md-12">
+        <div class="col-auto text-end">
+            <form action="questionario.php">
+                <button type="submit" class="btn btn-primary ">Confirm identity</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 
 <?php include "footer.php"; ?>
