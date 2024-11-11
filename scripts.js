2c32d7e2d5ef21f05de847f0a072977e2b4bf5c4
Thais <stanchackthais@gmail.com>
2024-11-11 17:45:37 -0300

    Arrumando o questionario

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function exibeCampo3(elem){
+
+    if(elem.value == 1){
+        $('#3_obsdiv').removeAttr('hidden');
+    } else{
+        $('#3_obsdiv').attr('hidden', true);
+    }
+}
+
+function exibeCampo4(elem){
+
+    if(elem.value == 1){
+        $('#4_obsdiv').removeAttr('hidden');
+    } else{
+        $('#4_obsdiv').attr('hidden', true);
+    }
+}
+
+
+function exibeCampo5(elem){
+
+    if(elem.value == 1){
+        $('#5_obsdiv').removeAttr('hidden');
+    } else{
+        $('#5_obsdiv').attr('hidden', true);
+    }
+}
