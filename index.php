2c0a300f660e1a7919fa67d3a09b46d046fbf0b9
Thais <stanchackthais@gmail.com>
2024-11-19 17:45:24 -0300

    Arrumando o index principal

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <?php include "header.php"; ?>
 
-
-
-<div class="container position-absolute top-50 start-50 translate-middle">
-    <div class="row">
-        <div class="col-6 " style="background-color: blue;">
-            <div class="border border-black rounded-5" style="background-color: yellow; width: 400px; height: 550px;">
-                <p>Nome Fantasia</p>
+<div class="container" style="margin-top: 10vh; width:50%;">
+    <div class="row text-center overflow-hidden" style="height: 64vh;">
+        <div class="col-12 col-md-6" style="background-color: #ccc; padding:2rem;">
+            <div class="bg-white rounded-2 " style="height: 55vh;">
+                <p>nome fantasia</p>
             </div>
         </div>
-        <div class="col-6 px-5 py-5" style="background-color: red">
-            <div class="row mb-5">
-                <div style="background-color: yellow;">
-                    <p>Nome Fantasia</p>
+        <div class="col-12 col-md-6">
+            <div class="row" style="height: 55vh; ">
+                <div class="col-12 col-md-12"
+                    style="background-color: #ccc; padding-top:2rem; padding-bottom:0rem; padding-right:2rem; padding-left:2rem;">
+                    <div class="bg-white rounded-2 pt-1" style="height: 26vh;">
+                        <p>Medico</p>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div style="background-color: yellow;">
-                    <p>Nome Fantasia</p>
+                <div class=" col-12 col-md-12"
+                    style="background-color: #ccc; padding-top:1.5rem; padding-bottom:2rem; padding-right:2rem; padding-left:2rem;">
+                    <div class="bg-white rounded-2 pt-1" style="height: 26vh;">
+                        <p>Paciente</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 
 
 <?php include "footer.php" ?>
