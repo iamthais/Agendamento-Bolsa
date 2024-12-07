487e16dc6fb16420801b675d02d8886af307bb97
iamthais <stanchackthais@gmail.com>
2024-12-06 21:45:39 -0300

    tela final

--- telafinal.php
+++ telafinal.php
@@ -1,31 +1,17 @@
 <?php include "header.php" ?>
 
 
-<div class="container mt-4
- telaFinal">
+<div class="container mt-5" style="background-color: #D9D9D9; border-radius: 2rem; padding: 5%;">
     <div class="row">
-        <div class="row">
-            <div class=" col-12 col-md-1"></div>
-            <div class=" col-12 col-md-5 mt-4 texto1 ">
-                <p>
-                    Seu pré-cadastro foi enviado com <strong>sucesso</strong>!
-                </p>
-            </div>
-
+        <div class="col-6">
+            <h1 style="font-size: 4rem;">Seu pré-cadastro<br> foi enviado com<br>sucesso!</h1>
+            <h1 style="font-size: 2.1rem; margin-top: 10%;">Agora basta ir ao<br> <b>[nome hospital]</b>,<br> para ser atendido</h1>
         </div>
-        <div class="row">
-            <div class="col-12 col-md-1"></div>
-            <div class="col-12 col-md-3 mt-4 texto2">
-                <p>
-                    Agora basta ir ao <strong>"Nome do Hospital"</strong> para ser atendido.
-                </p>
-            </div>
-        </div>
-        <div class="col-12 col-md-6">
+        <div class="col-6 text-center">
             <img class="Gato" src="img/gato.png">
+            <p>[icone] Texto de exemplo</p>
         </div>
     </div>
 </div>
-
 
 <?php include "footer.php" ?>
