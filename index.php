2f6ca16a892cb8921c710d03b084e7c6629192a5
Thais <stanchackthais@gmail.com>
2024-12-10 17:43:25 -0300

    arrumando href do paciente na imagem

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "header.php"; ?>
 
 <div class="container overflow-auto celular" style="width:50%;">
-    <div class="row rounded justify-content-end px-4 py-4" style="background-color: #ccc;">
+    <div class="row rounded justify-content-end px-4 py-4" style="background-color: #d1d1d1;">
         <!-- DIV NOME FANTASIA -->
         <div class="col-12 col-md-6" style="height: 30rem; width: 20rem;">
             <img src="img/nome_fantasia.png" style="width: 100%; height: 100%;">
@@ -10,15 +10,13 @@
         <div class="col-12 col-md-6">
             <div class="row">
                 <!-- DIV DO MÉDICO -->
-                <div class="col-12 col-md-12"
-                    style="height: 14.5rem; width: 20rem;">
+                <div class="col-12 col-md-12" style="height: 14.5rem; width: 20rem;">
                     <a href="medico/login.php"><img src="img/medico.png" style="width: 100%; height: 100%;"></a>
                 </div>
 
                 <!-- DIV DO PACIENTE -->
-                <div class="col-12 col-md-12"
-                    style="height: 14.5rem; width: 20rem; margin-top: 1rem">
-                    <a href="medico/login.php"><img src="img/paciente.png" style="width: 100%; height: 100%;"></a>
+                <div class="col-12 col-md-12" style="height: 14.5rem; width: 20rem; margin-top: 1rem">
+                    <a href="dadospessoais.php"><img src="img/paciente.png" style="width: 100%; height: 100%;"></a>
                 </div>
             </div>
         </div>
