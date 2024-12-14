842692b2020bfc8bfdad6fdeae295f5b8d99b5af
Natipaluch <natipaluch2@gmail.com>
2024-12-13 21:42:26 -0300

    mudei não sei

--- telainicial.php
+++ telainicial.php
@@ -2,22 +2,23 @@
 <?php include "header.php"; ?>
 <div class="container mt-5 texto1">
     <div class="row">
-        <div class="h1 col-md-8 " style="font-size: 70px;">
+        <div class="h1 col-md-8 " style="font-size: 60px;">
             <p>
                 Poupe <b>filas</b>,<br>realize o <br><b> pré-cadastro</b><br>
                 da sua <br> consulta hospitalar
             </p>
         </div>
             <div class="col-md-4 imagem" style="width: 400px;">
-                <img class="text-end cruzinha" src="img/Cruz.png" alt="imagemcruz" style="width:400px;">
+                <img class="text-end cruzinha" src="img/Cruz.png" alt="imagemcruz" style="width:250px;">
             </div>
-    </div>
-    <div class="row">
-        <div class="h3 col-md-12 text-center mt-5">
-        <form action="termosaceite.php">
-        <button type="submit" class="btn btn-primary" style="font-size: 25px;">Iniciar pré-cadastro</button>
+    
+        <div class="row">
+            <div class="h3 col-md-12 text-center mt-5">
+                <form action="termosaceite.php">
+                <button type="submit" class="btn btn-primary" style="font-size: 18px;">Iniciar pré-cadastro</button>
             
         
+            </div>
         </div>
     </div>
 </div>
