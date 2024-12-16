6c4aca8f11bb592975f569890cc5f2b6cc2244b9
iamthais <stanchackthais@gmail.com>
2024-12-15 22:16:13 -0300

    ajustando o index

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <head>
   <!-- Meta tags Obrigatórias -->
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
   <!-- Bootstrap CSS -->
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="scroll" style="padding-bottom: 100px;">
     <div class="container mt-5 texto1">
         <div class="row">
-            <div class=" col-md-8  " >
+            <div class="col-12 col-md-8" >
                 <h1 class="textotelainicial">
                     Poupe <b>filas</b>,<br>realize o <br><b> pré-cadastro</b><br>
                     da sua <br> consulta hospitalar
@@ -14,9 +14,10 @@
             </div>
         </div>
         <div class="row">
-            <div class="h3 col-md-12 text-center mt-5">
+            <div class="col-md-12 text-center mt-5">
                 <form action="termosaceite.php">
                     <button type="submit" class="btn btn-primary" style="font-size: 25px;">Iniciar pré-cadastro</button>
+                </form>
             </div>
         </div>
     </div>
